f1f2c10d49b61d73fa9fc4fa9d422fb573776721
youyeonjun <ceedar123@gmail.com>
2021-10-19 21:52:00 +0900

    2021-10-19 입생로랑 탑바

---
+++ saint_laurent/index.js
@@ -0,0 +1,9 @@
+function MenuBar__init() {
+    $('.menu-bar .menu-box-1 > ul > li').click(function () {
+        var text = $(this).find(' > a').text();
+
+        alert("당신은 " + text + "을(를) 클릭했습니다.");
+    });
+}
+
+MenuBar__init();
