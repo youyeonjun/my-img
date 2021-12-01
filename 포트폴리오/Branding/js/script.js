d358e696aa31585860a2c27fae6917eecbb759bd
youyeonjun <ceedar123@gmail.com>
2021-12-01 22:53:15 +0900

    2021-12-01 포플

--- 포트폴리오/Branding/js/script.js
+++ 포트폴리오/Branding/js/script.js
@@ -15,15 +15,7 @@ $(document).ready(function(){
     trigger.add('[data-trigger1]');
     trigger.add('[data-trigger2]');
 
-    new fullpage('#fullpage', {
-    navigation: true,
-    responsiveWidth: 700,
-    anchors: ['home', 'about-us', 'contact'],
-    parallax: true,
-    onLeave: function(origin, destination, direction){
-        console.log("Leaving section" + origin.index);
-    },
-});
+    
 
     $(".header-wrap > .header-box > ul > li").mouseover(function(){
         
